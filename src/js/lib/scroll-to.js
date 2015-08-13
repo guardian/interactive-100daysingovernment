56fecc9e786e29d03b73575d8796d99c982d2e2e
Will Franklin <will.franklin@guardian.co.uk>
2015-08-13 16:42:41 +0100

    Polyfill rAF

--- src/js/lib/scroll-to.js
+++ src/js/lib/scroll-to.js
@@ -4,18 +4,26 @@ function getOffset(el) {
     return el ? el.offsetTop + getOffset(el.offsetParent) : 0;
 }
 
+var animationFrame = window.requestAnimationFrame;
+var vendors = ['ms', 'moz', 'webkit', 'o'];
+for (var x = 0; x < vendors.length && !animationFrame; ++x) {
+    animationFrame = window[vendors[x]+'RequestAnimationFrame'];
+}
+
+animationFrame = animationFrame || function (cb) { setTimeout(cb, 13); };
+
 export default function scrollTo(el) {
     var start = window.pageYOffset;
     var end = getOffset(el);
     var distance = end - start;
     var elapsed = 0;
 
-    window.requestAnimationFrame(function scrollHandler() {
+    animationFrame(function scrollHandler() {
         var t = elapsed / total;
         window.scrollTo(0, Math.floor(start + distance * t * (2 - t)));
         if (elapsed < total) {
             elapsed += interval;
-            window.requestAnimationFrame(scrollHandler);
+            animationFrame(scrollHandler);
         }
     });
 };
