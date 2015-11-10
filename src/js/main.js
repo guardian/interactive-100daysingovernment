385d14127be12016564f93246f94937836078e2e
cfen <chris.fenn@guardian.co.uk>
2015-11-10 12:51:25 +0000

    Added color variation to border and font and resized font for counting down through list

--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,13 @@ import sectionHTML from './text/section.html!text'
 
 import scrollTo from './lib/scroll-to'
 import share from './lib/share'
+import getQueryVariable from './lib/get-query-var'
 
 
-var sheetKey = '1iTB3A14lnsKiSnCbaJTsFwS_sPEFXvwk5RoLMaRUKbs';
 const sheetPath = 'http://interactive.guim.co.uk/docsdata/';
+const sheetKey = getQueryVariable('key');
 const sheetFileType = '.json';
-
 const sheetURL = sheetPath+sheetKey+sheetFileType;
-
-console.log(sheetKey);
-
-//1Dqoxw3RUv0JSXkn5kXcMS0sa57KLjfmffNVZgk8pAts
 
 const sectionIds = ['A', 'B', 'C', 'D', 'E', 'F'];
 const sectionTitles = {
@@ -28,15 +24,16 @@ const sectionTitles = {
     'F': '51-60'
 };
 
+var totalCount;
+var baseLum = 0.075;
+
 var filterTemplateFn = doT.template(filterHTML);
 var sectionTemplateFn = doT.template(sectionHTML);
 
 var $$ = (el, s) => [].slice.apply(el.querySelectorAll(s));
 
-function app(el, days) {
-
-    console.log(el)
-
+function app(el, days, headInfo) {
+    totalCount = days.length;
     var sectionDays = {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': []};
     days.forEach(day => {
         if ((day.hierarchy === 'M' || day.hierarchy === 'H') && !day.imageHL) {
@@ -46,6 +43,8 @@ function app(el, days) {
             sectionDays[day.section].push(day);
         }
     });
+
+    
 
     var sectionsHTML = sectionIds.map(function (sectionId) {
         return sectionTemplateFn({
@@ -64,8 +63,85 @@ function app(el, days) {
         });
     });
 
-    document.querySelector('.l-footer').style.display = 'block';
+    setTitleColor(headInfo);
+
+   // document.querySelector('.l-footer').style.display = 'block';
+
+    
+    
 }
+
+function setTitleColor(headInfo){
+    headInfo.forEach(item => {
+        if(item.Type === 'PageHeader'){
+              document.getElementById("gv-pageHeading").innerHTML = item.Title;
+              // document.getElementById("gv-pageHeading").style.background = baseColor;
+        }
+        if(item.Type === 'Section'){
+              setColorScheme(setBaseColor(item.Title));
+              // document.getElementById("gv-pageHeading").style.background = baseColor;
+        }
+    });
+}
+
+function setBaseColor(v){
+
+    var c = "#194377";               
+        
+        if (v == "culture"){ c = "#951c55"}
+        if (v == "comment"){ c = "#c05303"}
+        if (v == "multimedia"){ c = "#484848"}
+        if (v == "sport"){ c = "#1C4A00"}
+  
+    return c;
+                        
+} 
+
+function setColorScheme(baseColor){
+    document.getElementById("filterArea").style.background = ColorLuminance(baseColor,baseLum);
+    document.getElementById("filterAreaBG").style.background = baseColor;
+
+
+    
+    // document.getElementById("fixedFilters").style.background = baseColor;
+    // document.getElementById("fixedFiltersBG").style.background = baseColor;
+    // document.getElementById("featureAreaBG").style.background = ColorLuminance(baseColor, baseLum);
+    // document.getElementById("featureArea").style.background = ColorLuminance(baseColor, baseLum);
+
+    var a = document.getElementsByClassName("dig-filters__filter__link__circle");
+    [].forEach.call(a, function (item) { item.style.color = baseColor; });
+
+    var a2 = document.getElementsByClassName('dig-days_day__number');
+    [].forEach.call(a2, function (item) {  var n = parseInt(item.getAttribute('data-count')); n = (11-n); var c = ColorLuminance(baseColor, n*baseLum);  item.style.color = c; });
+
+    var a3 = document.getElementsByClassName('dig-days__day');
+    [].forEach.call(a3, function (item) {  var n = parseInt(item.getAttribute('data-count')); n = (11-n); var c = ColorLuminance(baseColor, n*baseLum);  item.style.borderTop='1px solid #'+c });
+    //border-top: 1px solid #94bfdd;
+
+//     var color = window.getComputedStyle(
+//     document.querySelector('.element'), ':before'
+// ).getPropertyValue('color')
+} 
+
+function ColorLuminance(hex, lum) {
+                
+                hex = String(hex).replace(/[^0-9a-f]/gi, ''); // validate hex string
+
+                if (hex.length < 6) {
+                    hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+                }
+                lum = lum || 0;
+                
+                // convert to decimal and change luminosity
+                var rgb = "#", c, i;
+                for (i = 0; i < 3; i++) {
+                    c = parseInt(hex.substr(i*2,2), 16);
+                    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+                    rgb += ("00"+c).substr(c.length);
+                }
+
+                return rgb;
+            }
 
 export function init(el, context, config, mediator) {
     el.innerHTML = mainHTML;
@@ -96,6 +172,6 @@ export function init(el, context, config, mediator) {
         url: sheetURL,
         type: 'json',
         crossOrigin: true,
-        success: resp => app(el, resp.sheets.Sheet1)
+        success: resp => app(el, resp.sheets.Sheet1, resp.sheets.SheetCopy)
     });
 }
